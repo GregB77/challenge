895ce692c4774da4b34b991978e073055789a569
GregB77 <111422761+GregB77@users.noreply.github.com>
2025-10-15 10:21:46 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 // Remplace par la configuration de ton projet Firebase
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBHObOo2McUBg5UerkrQxMl7GEwGJnNiBw",
+  authDomain: "gregoire-ballandras.firebaseapp.com",
+  databaseURL: "https://gregoire-ballandras-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "gregoire-ballandras",
+  storageBucket: "gregoire-ballandras.firebasestorage.app",
+  messagingSenderId: "1095184130753",
+  appId: "1:1095184130753:web:a7ff3b5e7bf6facdd16a40",
+  measurementId: "G-XX39HNRM4H"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
